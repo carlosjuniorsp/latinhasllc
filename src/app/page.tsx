50dd230386b27c2afd362095ed669e0d90f7d17c
Carlos <carlosdossantosjuniorsp@gmail.com>
2023-10-07 14:44:21 -0300

    inserting editable columns and save and cancel buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 createContext('defaultValue');
 
 const rows: GridRowsProp = [
-  { id: 1, col1: 'Hello', period: '23/05/2022 - 29/05/2022', sku: 'World', total_plan: "5000", total_prod: 0 },
+  { id: 1, edit: 'EDITAR', period: '23/05/2022 - 29/05/2022', sku: 'World', total_plan: "5000", total_prod: 0 },
   { id: 2, col1: 'DataGridPro', period: '23/05/2022 - 29/05/2022', sku: 'is Awesome', total_plan: "5000", total_prod: 2600 },
   { id: 3, col1: 'MUI', period: '23/05/2022 - 29/05/2022', sku: 'is Amazing', total_plan: "5000", total_prod: 5000 },
 ];
 
 const columns: GridColDef[] = [
-  { field: 'col1', headerName: 'Column 1', width: 150, headerAlign: 'center', align: 'center' },
-  { field: 'period', headerName: 'PERÍODO', width: 150, headerAlign: 'center', align: 'center' },
-  { field: 'sku', headerName: 'SKUs', width: 150, headerAlign: 'center', align: 'center' },
-  { field: 'total_plan', headerName: 'TOTAL PLAN (TONS)', width: 200, headerAlign: 'center', align: 'center' },
-  { field: 'total_prod', headerName: 'TOTAL PROOD. (TONS)', width: 200, headerAlign: 'center', align: 'center' },
+  { field: 'edit', headerName: 'Column 1', width: 150, headerAlign: 'center', align: 'center', editable: true },
+  { field: 'period', headerName: 'PERÍODO', width: 150, headerAlign: 'center', align: 'center', editable: true },
+  { field: 'sku', headerName: 'SKUs', width: 150, headerAlign: 'center', align: 'center', editable: true },
+  { field: 'total_plan', headerName: 'TOTAL PLAN (TONS)', width: 200, headerAlign: 'center', align: 'center', editable: true },
+  { field: 'total_prod', headerName: 'TOTAL PROOD. (TONS)', width: 200, headerAlign: 'center', align: 'center', editable: true },
 ];
 export default function Home() {
   return (
@@ -29,6 +29,8 @@ export default function Home() {
         }}
           pageSizeOptions={[2, 4, 6]}
         />
+        <button className="btn-cancel float-right mt-10 bg-green-600  hover:bg-green-500 ml-2 text-white font-bold py-2 px-4 rounded">Salvar</button>
+        <button className="btn-save float-right mt-10 bg-red-600 hover:bg-red-500 text-white font-bold py-2 px-4 rounded">Cancelar</button>
       </div>
     </div>
   );
